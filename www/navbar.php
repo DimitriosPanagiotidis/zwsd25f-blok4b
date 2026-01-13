41da43b4a6452095ebfe267e658851ef91a43312
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2026-01-13 11:04:26 +0100

    Add initial files for logout, navbar, registration, and reset CSS

---
+++ www/navbar.php
@@ -0,0 +1,29 @@
+<!-- <nav class="navigation-bar">
+    <ul>
+        <li><a href="index.php">Home</a></li></li>
+        <li><a href="artworks.php">Artworks</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
+ -->
+
+
+<header class="navbar">
+    <div class="navbar-brand">ARTSPACE</div>
+
+    <nav class="navbar-links" id="navLinks">
+        <a href="#" class="active">Home</a>
+        <a href="#">Gallery</a>
+        <a href="#">Artists</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+    </nav>
+
+    <button class="hamburger" id="hamburger" aria-label="Menu">
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
+</header>
+<script src="script.js"></script>
