40931a39ea5f7b61b8697547d8d646be5c471513
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2026-01-16 18:29:49 +0100

    Update user stories document and remove obsolete image files; enhance artwork display in artworks.php

--- www/artworks.php
+++ www/artworks.php
@@ -6,25 +6,24 @@ require 'database.php';
 // $artworks is already coming from index.php
 
 ?>
-            <section class="artworks">
-                <?php
-            foreach ($artworks as $artwork): ?>
-                <?php $img = $artwork['image']; ?>
-                <div>
-                <h2><?php echo $artwork['title']; ?></h2>
-                <p><?php echo "Artist: " . $artwork['artist']; ?></p>
-                <p><?php echo "Year: " . $artwork['year_created']; ?></p>
-                <p><?php echo "Price: $" . $artwork['price']; ?></p>
-                <?php // Substr 0 dus begint vanaf het begin en -4 om de laatste 4 karakters (.jpg) te verwijderen ?>
-                <a href="detailpage.php?Id=<?php echo $artwork['Id']; ?>">
-
-            
-                <?php if(file_exists(__DIR__ . '/' . substr($img, 0, -4) . "_small.jpg")): ?>
+<section class="artworks">
+    <?php
+    foreach ($artworks as $artwork): ?>
+        <?php $img = $artwork['image']; ?>
+        <div>
+            <h2><?php echo $artwork['title']; ?></h2>
+            <p><?php echo "Artist: " . $artwork['artist']; ?></p>
+            <p><?php echo "Year: " . $artwork['year_created']; ?></p>
+            <p><?php echo "Price: $" . $artwork['price']; ?></p>
+            <?php // Substr 0 dus begint vanaf het begin en -4 om de laatste 4 karakters (.jpg) te verwijderen 
+            ?>
+            <a href="detailpage.php?Id=<?php echo $artwork['Id']; ?>">
+                <?php if (file_exists(__DIR__ . '/' . substr($img, 0, -4) . "_small.jpg")): ?>
                     <img src="<?php echo substr($img, 0, -4) . "_small.jpg"; ?>" alt="<?php echo $artwork['title']; ?>" />
                 <?php else: ?>
                     <img src="<?php echo  $img; ?>" alt="<?php echo $artwork['title']; ?>" />
                 <?php endif; ?>
-                    </a>
-                </div>
-            <?php endforeach; ?>
-        </section>
+            </a>
+        </div>
+    <?php endforeach; ?>
+</section>
