369d9b19eb9301bd03a00e3d460329aa21e6fcd2
DimitriosPanagiotidis <dimitrispanagiotidis2003@gmail.com>
2026-01-16 12:36:18 +0100

    Refactor index.php to include price filtering for artworks, enhance welcome message with username and last login, and improve layout structure.

--- www/index.php
+++ www/index.php
@@ -1,6 +1,23 @@
 <?php
 
 include 'session_check.php';
+require 'database.php';
+
+$sql = "SELECT * FROM artwork";
+
+//Filter on price
+if(isset($_GET['price'])){
+    if($_GET['price'] === 'asc'){
+        $sql = "SELECT * FROM artwork ORDER BY CAST(price AS DECIMAL(10,2)) ASC";
+    }
+    elseif($_GET['price'] === 'desc'){
+        $sql = "SELECT * FROM artwork ORDER BY CAST(price AS DECIMAL(10,2)) DESC";
+    }
+}
+
+$result = mysqli_query($conn, $sql);
+$artworks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,18 +30,37 @@ include 'session_check.php';
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    <header>
+    <header class="index_header">
         <?php include 'navbar.php'; ?>
+            <div class="welcome_message">
+                <h2>Welcome back! <span><?php echo $_SESSION['username']; ?></span></h2>
+            </div>
+        <p><?php echo $_SESSION['last_login']; ?></p>
     </header>
-    <main>
+    <section>
         <div class="index-main">
-            <h1>Welcome to ArtSpace</h1>
-            <p>Your gateway to the world of art.</p>
+            <h1>ArtSpace</h1>
+            <h2>Your gateway to the world of art.</h2>
         </div>
-    </main>
+    </section>
     <main>
+        <!-- Filters -->
+        <section class="filters">
+            <ul>
+                <li>
+                    <a href="index.php">All Artworks</a>
+                </li>
+                <li>
+                    <a href="?price=asc">Price Low - High</a>
+                </li>
+                <li>
+                    <a href="?price=desc">Price High - Low</a>
+                </li>
+            </ul>
+        </section>
+
+
         <?php include 'artworks.php'; ?>
     </main>
     <?php include 'footer.php'; ?>
